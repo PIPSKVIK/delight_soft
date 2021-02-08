643758854df7c267e1b179dbba8f5149cbc7d7d5
Alexander <milovanovska@yandex.ru>
2021-02-08 14:31:02 +0300

    create project

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,88 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div class="container">
+      <div class="container__body">
+        <div class="container__info">
+          <button @click="localFileInfo = []" class="container__info-button">clear info</button>
+          <span>FILE INFORMATION</span>
+          <ul v-for="(item, id) in localFileInfo" :key="id">
+            <li>{{ item }}</li>
+          </ul>
+        </div>
+        <tree-browser :node="files" @onClick="nodeHeandler"/>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
+import TreeBrowser from "@/components/Children";
 export default {
   name: "App",
-  components: {
-    HelloWorld
+  components: { TreeBrowser },
+  data() {
+    return {
+      files: [],
+      localFileInfo: []
+    };
+  },
+  mounted() {
+    fetch("http://localhost:3000/root")
+      .then(response => response.json())
+      .then(json => {
+        this.files = json;
+      });
+  },
+  methods: {
+    nodeHeandler(node) {
+      this.localFileInfo = node;
+    }
   }
 };
 </script>
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  height: 100vh;
+  background-color: #585858;
+}
+.container {
+  &__body {
+    display: flex;
+    padding: 10px;
+  }
+
+  &__title {
+    text-align: center;
+  }
+
+  &__info {
+    color: white;
+    background-color: gray;
+    width: 400px;
+    min-height: 200px;
+    border-radius: 5px;
+    position: relative;
+
+    &-button {
+      position: absolute;
+      top: 10px;
+      right: 10px;
+    }
+
+    & ul {
+      text-align: left;
+      list-style: none;
+      margin: 0;
+    }
+    & li {
+      margin-bottom: 10px;
+    }
+  }
+}
+
+body {
+  margin: 0;
 }
 </style>
---
+++ src/components/Children.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="children">
+    <div
+      class="node"
+      @click="nodeHeandler"
+    >
+      <span class="node__folder" v-if="lastElement"
+        >{{ expanded ? "&#128505;" : "&#65794;" }}
+      </span>
+      <span v-else>&#10022;</span>
+      {{ node.name }}
+    </div>
+    <div v-if="expanded">
+      <TreeBrowser
+        v-for="child in node.under_folder"
+        :key="child.name"
+        :node="child"
+        @onClick="node => $emit('onClick', node)"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TreeBrowser",
+  props: {
+    node: [Array, Object]
+  },
+  data() {
+    return {
+      expanded: false
+    };
+  },
+  computed: {
+    lastElement() {
+      return this.node.under_folder;
+    }
+  },
+  methods: {
+    nodeHeandler() {
+      this.expanded = !this.expanded;
+      if (!this.lastElement) {
+        this.$emit("onClick", this.node);
+      }
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.children {
+  display: flex;
+  margin-left: 10px;
+}
+.node {
+  height: 40px;
+  min-width: 100px;
+  display: flex;
+  align-items: center;
+  font-size: 18px;
+  cursor: pointer;
+  color: #3fb984;
+  margin-right: 20px;
+  margin-bottom: 10px;
+  border: 1px solid #3fb984;
+  border-radius: 5px;
+
+  &__folder {
+    margin-right: 10px;
+  }
+}
+</style>
